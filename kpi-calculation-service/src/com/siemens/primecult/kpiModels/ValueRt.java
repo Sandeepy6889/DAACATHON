4e31266850c32453e17dff09d22e81c6694d519c
Shivani Bansal <shivani.bansal@siemens.com>
2018-03-28 15:17:14 +0530

    kpi-calculation rest service added

---
+++ kpi-calculation-service/src/com/siemens/primecult/kpiModels/ValueRt.java
@@ -0,0 +1,40 @@
+package com.siemens.primecult.kpiModels;
+
+public class ValueRt {
+
+	private String assetID;
+
+	private long timeStamp;
+
+	private double[] values;
+
+	public ValueRt(String assetID, long timeStamp, double[] values) {
+		this.setAssetID(assetID);
+		this.setTimeStamp(timeStamp);
+		this.setValues(values);
+	}
+
+	public String getAssetID() {
+		return assetID;
+	}
+
+	public void setAssetID(String assetID) {
+		this.assetID = assetID;
+	}
+
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public double[] getValues() {
+		return values;
+	}
+
+	public void setValues(double[] values) {
+		this.values = values;
+	}
+}
