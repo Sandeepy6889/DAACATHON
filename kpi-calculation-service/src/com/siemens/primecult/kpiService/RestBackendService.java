ce57fc379cf408ce346c414acab8d9dfddfacecc
Shivani Bansal <shivani.bansal@siemens.com>
2018-03-28 16:36:23 +0530

    lib folder checkin

--- kpi-calculation-service/src/com/siemens/primecult/kpiService/RestBackendService.java
+++ kpi-calculation-service/src/com/siemens/primecult/kpiService/RestBackendService.java
@@ -2,13 +2,14 @@ package com.siemens.primecult.kpiService;
 
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
-@Path("/KPI-Calculation")
+@Path("/KPI-Calculation/*")
 public class RestBackendService {
 
 	@POST
@@ -21,6 +22,16 @@ public class RestBackendService {
 		return plainText;
 	}
 	
+	
+	@GET
+	@Path("/test")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String getKPI() {
+		//Object obj = new ObjectToJSonMapper().mapJSonStrToObj(plainText,ValueRt.class);
+		System.out.println("HELLO");
+		return "Hello";
+	}
 /*	private void getValueRt() throws IOException {
 		 double[] v=  {9.0, 0.28614743881722116, 0.0, 0.0};
 		ValueRt val = new ValueRt("1", 13166690909000l, v);
