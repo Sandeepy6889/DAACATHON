7c6127f619536971d40e7bed81ebf92d66bc83fd
Siemens123# <sandeepy6889@gmail.com>
2018-04-14 18:35:46 +0530

    updated kpi calc logic

--- kpi-calculation-service/src/com/siemens/primecult/alarmservices/AlarmManagement.java
+++ kpi-calculation-service/src/com/siemens/primecult/alarmservices/AlarmManagement.java
@@ -3,15 +3,14 @@ package com.siemens.primecult.alarmservices;
 import static com.siemens.primecult.alarmservices.KPIAlarmService.checkKpiStateChange;
 import static com.siemens.primecult.alarmservices.PreventiveAlarmService.checkPreventiveAlarmStateChange;
 import static com.siemens.primecult.alarmservices.VibrationAlarmService.checkVibrationAlarmStateChange;
-
 import static com.siemens.primecult.constants.AlarmStatus.GONE;
-import static com.siemens.primecult.constants.AlarmStatus.RAISED;
 import static com.siemens.primecult.constants.AlarmStatus.NOT_DEFINED;
+import static com.siemens.primecult.constants.AlarmStatus.RAISED;
 import static com.siemens.primecult.constants.AlarmTypes.BLOCKAGE;
 import static com.siemens.primecult.constants.AlarmTypes.DRYRUN;
 import static com.siemens.primecult.constants.AlarmTypes.EFFICIENCY;
-import static com.siemens.primecult.constants.AlarmTypes.TDH;
 import static com.siemens.primecult.constants.AlarmTypes.IMPELLER_WEAR_COMBINATION;
+import static com.siemens.primecult.constants.AlarmTypes.TDH;
 import static com.siemens.primecult.constants.PumpMonitorConstant.DISCH_PRESSURE;
 import static com.siemens.primecult.constants.PumpMonitorConstant.FLUID_FLOW_RATE;
 import static com.siemens.primecult.constants.PumpMonitorConstant.MOTOR_POWER_INPUT;
@@ -24,7 +23,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -44,6 +42,7 @@ public class AlarmManagement {
 
 	private Connection connection;
 	InsertOperations inOperation = new InsertOperations();
+
 	public AlarmManagement(Connection connection) {
 		this.connection = connection;
 	}
@@ -58,7 +57,7 @@ public class AlarmManagement {
 		currentAlarmsStatus.put(assetId, Arrays.asList(GONE, GONE, GONE, GONE, GONE));
 		return "success";
 	}
-	
+
 	public static String removeEntryOfAssetFromAlarmManagement(String assetId) {
 		if (currentAlarmsStatus.get(assetId) == null)
 			return "asset not configured";
@@ -66,49 +65,48 @@ public class AlarmManagement {
 		return "success";
 	}
 
-	public void setAlarmsState(KPIData calculatedKPI, KPIData refrencedKPI, ValueRt rawValue, boolean isAssetTrained) throws SQLException {
-		
+	public void setAlarmsState(KPIData calculatedKPI, KPIData refrencedKPI, ValueRt rawValue, boolean isAssetTrained)
+			throws SQLException {
+
 		// get threshold from db
 		float threshold = fetchThresholdFromDb(rawValue.getAssetID());
 		// check for TDH alarm
 		if (isAssetTrained && checkKpiStateChange(calculatedKPI, refrencedKPI, threshold, TDH, rawValue.getAssetID(),
 				currentAlarmsStatus))
 			changeStateInCacheAndDb(TDH, rawValue);
-		else if(!isAssetTrained) 
+		else if (!isAssetTrained)
 			currentAlarmsStatus.get(rawValue.getAssetID()).set(TDH.getIndex(), NOT_DEFINED);
 
 		// check for efficiency alarm
 		if (isAssetTrained && checkKpiStateChange(calculatedKPI, refrencedKPI, threshold, EFFICIENCY,
 				rawValue.getAssetID(), currentAlarmsStatus))
 			changeStateInCacheAndDb(EFFICIENCY, rawValue);
-		else if(!isAssetTrained) 
+		else if (!isAssetTrained)
 			currentAlarmsStatus.get(rawValue.getAssetID()).set(EFFICIENCY.getIndex(), NOT_DEFINED);
 
 		// check for blockage alarm
-		if (checkPreventiveAlarmStateChange(BLOCKAGE, rawValue, currentAlarmsStatus))
-			{
-			changeStateInCacheAndDb(BLOCKAGE, rawValue);
-			int integer = currentAlarmsStatus.get(rawValue.getAssetID()).get(BLOCKAGE.getIndex());
-			if(RAISED == integer)
-			{
-				sendMailForAlarm(rawValue.getAssetID(),"BLOCKAGE");
+		if (checkPreventiveAlarmStateChange(BLOCKAGE, rawValue, currentAlarmsStatus)) {
+			System.out.println("checking to send mail blockage");
+			if (RAISED == changeStateInCacheAndDb(BLOCKAGE, rawValue)) {
+				System.out.println("sending mail blckage");
+				sendMailForAlarm(rawValue.getAssetID(), "BLOCKAGE");
 			}
-			}
-		
+		}
+
 		// check for dryrun alarm
-		if (checkPreventiveAlarmStateChange(DRYRUN, rawValue, currentAlarmsStatus))
-		{
-			changeStateInCacheAndDb(DRYRUN, rawValue);
-			int integer = currentAlarmsStatus.get(rawValue.getAssetID()).get(DRYRUN.getIndex());
-			if(RAISED == integer)
-			{
-				sendMailForAlarm(rawValue.getAssetID(),"DRYRUN");
+		if (checkPreventiveAlarmStateChange(DRYRUN, rawValue, currentAlarmsStatus)) {
+			System.out.println("checking to send mail dry");
+			if (RAISED == changeStateInCacheAndDb(DRYRUN, rawValue)) {
+				System.out.println("sending mail dry");
+				sendMailForAlarm(rawValue.getAssetID(), "DRYRUN");
 			}
 		}
 		// check for Impeller wear combination alarm
 		if (checkVibrationAlarmStateChange(IMPELLER_WEAR_COMBINATION, rawValue, currentAlarmsStatus))
 			changeStateInCacheAndDb(IMPELLER_WEAR_COMBINATION, rawValue);
 	}
+	
+	
 
 	private float fetchThresholdFromDb(String assetId) {
 		List<Object> values = DBUtil.getColumnValues("select threslt from paramdata where assetid='" + assetId + "'",
@@ -116,20 +114,21 @@ public class AlarmManagement {
 		return values.size() == 0 ? 0 : (Float) ((Pair) values.get(0)).getValue();
 	}
 
-	private void changeStateInCacheAndDb(AlarmTypes alarmType, ValueRt rawValues) throws SQLException {
+	private int changeStateInCacheAndDb(AlarmTypes alarmType, ValueRt rawValues) throws SQLException {
 		String assetId = rawValues.getAssetID();
 		int stateInCache = currentAlarmsStatus.get(assetId).get(alarmType.getIndex());
+		int newState = -1;
 		switch (stateInCache) {
 		case RAISED:
-			currentAlarmsStatus.get(assetId).set(alarmType.getIndex(), GONE);
+			newState = GONE;
 			break;
 		case GONE:
-			currentAlarmsStatus.get(assetId).set(alarmType.getIndex(), RAISED);
+			newState = RAISED;
 			break;
 		default:
-			currentAlarmsStatus.get(assetId).set(alarmType.getIndex(), GONE);
+			newState = GONE;
 		}
-
+		currentAlarmsStatus.get(assetId).set(alarmType.getIndex(), newState);
 		float[] measuredValues = rawValues.getKpiValues();
 		TableRow row = new TableRow("alarms");
 		row.set("asset_id", assetId);
@@ -141,20 +140,34 @@ public class AlarmManagement {
 		row.set("alarm_status", currentAlarmsStatus.get(assetId).get(alarmType.getIndex()));
 		row.set("timestamp", rawValues.getTimeStamp());
 		inOperation.insert(connection, row);
+		return newState;
 	}
-	private static void sendMailForAlarm(String assetId,  String raisedAlarm) {
+
+	private static void sendMailForAlarm(String assetId, String raisedAlarm) {
 		try {
-	        List<Object> columnValues = DBUtil.getColumnValues("select alarm_subs from urls_info", "alarm_subs");
-	        String eMailSubURL = ((Pair)columnValues.get(0)).getValue().toString();
-			URL url = new URL(eMailSubURL+"/publishMail/" + assetId + "/" + raisedAlarm);
+			List<Object> columnValues = DBUtil.getColumnValues("select alarm_subs from urls_info", "alarm_subs");
+			String eMailSubURL = ((Pair) columnValues.get(0)).getValue().toString();
+			URL url = new URL(eMailSubURL + "/publishMail/" + assetId + "/" + raisedAlarm);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setDoOutput(true);
-			conn.getResponseMessage();
+			String responseMessage = conn.getResponseMessage();
+			System.out.println(responseMessage);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			StringBuilder result = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				result.append(line);
+			}
+			System.out.println(result);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//return refValue;
+		// return refValue;
 	}
 	
+	public static void main(String[] args) {
+		sendMailForAlarm("PUMP03", "DRYRUN");
 	}
+
+}
